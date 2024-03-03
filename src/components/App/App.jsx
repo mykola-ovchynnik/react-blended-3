8de392832b8b2d9bf09ffe5d4df5dfb13354d99a
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2024-03-03 10:19:27 +0100

    render countries

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,17 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Header } from 'components';
 import { CountrySearch, Home, Country } from 'pages';
 
 export const App = () => {
-  return <h2>App</h2>;
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="country" element={<CountrySearch />} />
+        <Route path="country/:countryId" element={<Country />} />
+      </Route>
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/country-service';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    getCountries()
+      .then(setCountries)
+      .catch(error => setError(error.message))
+      .finally(setLoading(false));
+  }, []);
+
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        <CountryList countries={countries} />
+        {loading && <Loader />}
+        {error && <Heading>{error}</Heading>}
       </Container>
     </Section>
   );
